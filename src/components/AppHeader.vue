b004359110521099744d0c1783e55b669d14ae2f
mohdraid1990 <105244091+mohdraid1990@users.noreply.github.com>
2024-10-08 12:28:58 +0400

    Update AppHeader.vue

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -48,24 +48,23 @@
 
 <script>
 export default {
-  name: "AppHeader",
+  name: "AppHeader", // Название компонента
   data() {
     return {
-      sidebarVisible: false,
+      sidebarVisible: false, // Переменная для управления видимостью боковой панели
     };
   },
   methods: {
     toggleSidebar() {
-      this.sidebarVisible = !this.sidebarVisible;
-      const sidebar = document.getElementById("sidebar");
-      sidebar.classList.toggle("visible", this.sidebarVisible);
-      sidebar.classList.toggle("hidden", !this.sidebarVisible);
+      this.sidebarVisible = !this.sidebarVisible; // Переключаем состояние видимости боковой панели
+      const sidebar = document.getElementById("sidebar"); // Получаем элемент боковой панели по ID
+      sidebar.classList.toggle("visible", this.sidebarVisible); // Добавляем/удаляем класс 'visible' в зависимости от состояния
+      sidebar.classList.toggle("hidden", !this.sidebarVisible); // Добавляем/удаляем класс 'hidden' в зависимости от состояния
     },
   },
 };
 </script>
-
-<style scoped>
+<style lang="scss" scoped>
 header {
   grid-column: span 11 / span 11;
   background-color: var(--white-color);
@@ -85,12 +84,14 @@ nav {
   height: 60px;
 }
 
-.setting p {
-  font-family: Inter;
-  font-size: 28px;
-  font-weight: 600;
-  line-height: 33.89px;
-  color: var(--primary-color);
+.setting {
+  p {
+    font-family: Inter;
+    font-size: 28px;
+    font-weight: 600;
+    line-height: 33.89px;
+    color: var(--primary-color);
+  }
 }
 
 .info {
@@ -98,79 +99,79 @@ nav {
   align-items: center;
   justify-content: center;
   gap: 20px;
-}
 
-.search-inp {
-  position: relative;
-  display: flex;
-  justify-content: center;
-  width: 255px;
-  height: 50px;
-  background-color: var(--background-color);
-  border-radius: 40px;
-  padding: 5px;
-  align-items: center;
-}
+  .search-inp {
+    position: relative;
+    display: flex;
+    justify-content: center;
+    width: 255px;
+    height: 50px;
+    background-color: var(--background-color);
+    border-radius: 40px;
+    padding: 5px;
+    align-items: center;
 
-.search-inp img {
-  margin-top: 3px;
-  margin-left: 24px;
-  width: 20px;
-  height: 20px;
-}
+    img {
+      margin-top: 3px;
+      margin-left: 24px;
+      width: 20px;
+      height: 20px;
+    }
 
-.search-inp input {
-  border: none;
-  outline: none;
-  flex: 1;
-  text-align: right;
-  font-size: 16px;
-  background: none;
-  font-family: Inter;
-  font-size: 15px;
-  font-weight: 400;
-  line-height: 18.15px;
-  color: var(--text-color);
-  width: 152px;
-  height: 18px;
-  text-align: center;
-}
+    input {
+      border: none;
+      outline: none;
+      flex: 1;
+      text-align: right;
+      font-size: 16px;
+      background: none;
+      font-family: Inter;
+      font-size: 15px;
+      font-weight: 400;
+      line-height: 18.15px;
+      color: var(--text-color);
+      width: 152px;
+      height: 18px;
+      text-align: center;
 
-.search-inp input::placeholder {
-  font-family: Inter;
-  font-size: 15px;
-  font-weight: 400;
-  line-height: 18.15px;
-  color: var(--text-color);
-}
+      &::placeholder {
+        font-family: Inter;
+        font-size: 15px;
+        font-weight: 400;
+        line-height: 18.15px;
+        color: var(--text-color);
+      }
+    }
+  }
 
-.setting-info {
-  width: 50px;
-  height: 50px;
-  background-color: var(--background-color);
-  border-radius: 50%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  cursor: pointer;
-}
+  .setting-info {
+    width: 50px;
+    height: 50px;
+    background-color: var(--background-color);
+    border-radius: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
 
-.info .setting-info:last-child img {
-  border-radius: 50%;
-  width: 60px;
-  height: 60px;
+    &:last-child img {
+      border-radius: 50%;
+      width: 60px;
+      height: 60px;
+    }
+  }
 }
 
 .menu {
-  display: none;
+  display: none; // скрыть меню
 }
 
 .info-mobile {
-  display: none;
+  display: none; // скрыть мобильную информацию
 }
 
 .search-inp-mobile {
-  display: none;
+  display: none; // скрыть мобильное поле поиска
 }
 
 .info-mobile img {
@@ -203,10 +204,6 @@ nav {
   transform: rotate(180deg);
 }
 
-#menuIcon {
-  cursor: pointer;
-}
-
 @keyframes slideIn {
   from {
     transform: translateX(-100%);
@@ -237,89 +234,16 @@ nav {
   animation: slideOut 0.5s forwards;
 }
 
-/* Медиа-запрос для экранов шириной до 1024px */
-@media (max-width: 1024px) {
-  .container {
-    width: 650px !important; /* Ширина контейнера */
-  }
-  nav {
-    justify-content: space-around; /* Распределение элементов навигации */
-  }
-  form .img-form {
-    left: 32%; /* Положение формы изображения */
-    margin-top: 60px; /* Отступ сверху */
-  }
-  .form-group {
-    padding: 0 !important; /* Убираем отступы у группы форм */
-  }
-  .btn {
-    justify-content: center !important;
-
-    align-items: center !important;
-
-    width: auto;
-    height: auto;
-  }
-
-  side-bar .logo h1 {
-    font-size: 20px !important; /* Размер шрифта для логотипа */
-  }
-  .list {
-    margin-top: 5px !important; /* Отступ сверху для списка */
-  }
-  .list li a {
-    font-size: 15px !important; /* Размер шрифта для ссылок в списке */
-  }
-  .menu {
-    display: block; /* Показываем меню */
-    margin-left: 100px; /* Отступ слева */
-    cursor: pointer; /* Указатель курсора */
-    margin-top: 9px; /* Отступ сверху */
-  }
-  #closeIcon {
-    left: 60px; /* Положение иконки закрытия */
-  }
-  side-bar {
-    display: none; /* Скрываем боковую панель */
-  }
-  header {
-    grid-column: span 12 / span 10; /* Заголовок занимает 10 колонок */
-  }
-  .setting {
-    margin-left: 30px; /* Отступ слева для настроек */
-  }
-  #sidebar.hidden {
-    display: none; /* Скрываем боковую панель, если она скрыта */
-  }
-  .list {
-    width: 165px; /* Ширина списка */
-    height: 555px; /* Высота списка */
-    display: flex; /* Используем flexbox */
-    flex-wrap: wrap; /* Перенос элементов */
-    align-items: center; /* Выравнивание по центру */
-    margin-top: 90px; /* Отступ сверху */
-  }
-  side-bar .logo {
-    margin-top: 80px; /* Отступ сверху для логотипа */
-  }
-  #sidebar.visible {
-    display: block; /* Показываем боковую панель, если она видима */
-    position: absolute; /* Абсолютное позиционирование */
-    z-index: 1000; /* Уровень слоя */
-    background-color: var(--white-color); /* Цвет фона боковой панели */
-    height: 100%; /* Высота на весь экран */
-  }
-}
-
 /* Медиа-запрос для экранов шириной до 1440px */
 @media (max-width: 1440px) {
   header {
-    padding: 0; /* Убираем отступы у заголовка */
+    padding: 0; // Убираем отступы у заголовка
   }
 
   .line {
-    width: 550px; /* Ширина линии */
+    width: 550px; // Ширина линии
   }
+
   .btn {
     justify-content: flex-end;
     width: auto;
@@ -327,319 +251,147 @@ nav {
   }
 }
 
-/* Медиа-запрос для экранов шириной до 425px */
-@media (max-width: 450px) {
-  .line {
-    width: 285px; /* Ширина линии */
-  }
-  form .img-form {
-    left: 20%; /* Положение формы изображения */
-    margin-top: 18px; /* Отступ сверху */
-  }
-  header {
-    height: 140px; /* Высота заголовка */
-  }
-  .info {
-    display: none; /* Скрываем информацию */
-  }
-  nav {
-    margin-bottom: 45px; /* Отступ снизу для навигации */
-  }
-  .menu {
-    margin-bottom: 50px; /* Отступ снизу для меню */
-    margin-left: 29px; /* Отступ слева для меню */
-  }
-  .search-inp-mobile {
-    display: block; /* Показываем мобильное поле поиска */
-    position: absolute; /* Абсолютное позиционирование */
-    top: 8%; /* Положение сверху */
-    left: 5%; /* Положение слева */
-  }
-  #closeIcon {
-    left: 143px; /* Положение иконки закрытия */
-  }
-  .search-inp-mobile input {
-    width: 325px; /* Ширина поля ввода */
-    height: 40px; /* Высота поля ввода */
-    border: none; /* Убираем границу */
-    outline: none; /* Убираем обводку */
-    border-radius: 40px; /* Скругление углов */
-    background-color: var(--background-color); /* Цвет фона поля ввода */
-    display: flex; /* Используем flexbox */
-    justify-content: center; /* Выравнивание по центру */
-    padding: 5px; /* Отступы */
-    align-items: center; /* Выравнивание по центру */
-  }
-  .search-inp-mobile img {
-    margin-bottom: -33px; /* Отступ снизу для изображения */
-    margin-left: 18px; /* Отступ слева для изображения */
-    width: 16px; /* Ширина изображения */
-    height: 16px; /* Высота изображения */
-  }
-  .search-inp-mobile input::placeholder {
-    font-family: Inter; /* Шрифт для плейсхолдера */
-    font-size: 13px; /* Размер шрифта */
-    font-weight: 400; /* Жирность шрифта */
-    line-height: 15.73px; /* Высота строки */
-    color: var(--text-color); /* Цвет плейсхолдера */
-    padding: 50px; /* Отступы */
-  }
-  .setting p {
-    font-family: Inter; /* Шрифт для текста настроек */
-    font-size: 20px; /* Размер шрифта */
-    font-weight: 600; /* Жирность шрифта */
-    line-height: 24.2px; /* Высота строки */
-    text-align: center; /* Выравнивание текста по центру */
-  }
-  .info-mobile img {
-    width: 35px; /* Ширина изображения мобильной информации */
-    height: 35px; /* Высота изображения мобильной информации */
-    border-radius: 50%; /* Скругление углов изображения */
-  }
-  .list {
-    margin-top: 10px; /* Отступ сверху для списка */
-  }
-  .list li a {
-    font-size: 15px; /* Размер шрифта для ссылок в списке */
-  }
-  .info-mobile {
-    display: block; /* Показываем мобильную информацию */
-  }
-  .name input,
-  .name select {
-    width: 280px; /* Ширина полей ввода и выбора */
-    height: 40px; /* Высота полей ввода и выбора */
-  }
-  .link-main a {
-    font-family: Inter; /* Шрифт для ссылок */
-    font-size: 13px; /* Размер шрифта */
-    font-weight: 500; /* Жирность шрифта */
-    line-height: 15.73px; /* Высота строки */
-  }
-  main .link-main {
-    gap: 30px; /* Промежуток между элементами */
-  }
-  .btn {
-    display: flex;
-    padding-top: 30px;
-    justify-content: center;
-    align-items: center;
-    width: auto; /* Ширина кнопки */
-    height: auto; /* Высота кнопки */
-  }
-  .btn button {
-    width: 285px; /* Ширина кнопки */
-    height: 40px; /* Высота кнопки */
-  }
-}
-
 /* Медиа-запрос для экранов шириной до 1024px */
 @media (max-width: 1024px) {
   nav {
-    justify-content: space-around; /* Распределение элементов навигации */
-  }
-  form .img-form {
-    left: 32%; /* Положение формы изображения */
-    margin-top: 60px; /* Отступ сверху */
-  }
-  .form-group {
-    padding: 0 !important; /* Убираем отступы у группы форм */
-  }
-  .btn {
-    justify-content: center !important;
-
-    align-items: center !important;
-
-    width: auto;
-    height: auto;
+    justify-content: space-around; // Распределение элементов навигации
   }
 
-  side-bar .logo h1 {
-    font-size: 20px !important; /* Размер шрифта для логотипа */
-  }
-  .list {
-    margin-top: 5px !important; /* Отступ сверху для списка */
-  }
-  .list li a {
-    font-size: 15px !important; /* Размер шрифта для ссылок в списке */
-  }
   .menu {
-    display: block; /* Показываем меню */
-    margin-left: 100px; /* Отступ слева */
-    cursor: pointer; /* Указатель курсора */
-    margin-top: 9px; /* Отступ сверху */
+    display: block; // Показываем меню
+    margin-left: 60px; // Отступ слева
+    cursor: pointer; // Указатель курсора
+    margin-top: 9px; // Отступ сверху
   }
+
   #closeIcon {
-    left: 60px; /* Положение иконки закрытия */
+    left: 143px; // Положение иконки закрытия
   }
-  side-bar {
-    display: none; /* Скрываем боковую панель */
-  }
-  header {
-    grid-column: span 12 / span 10; /* Заголовок занимает 10 колонок */
-  }
-  .setting {
-    margin-left: 30px; /* Отступ слева для настроек */
-  }
+
   #sidebar.hidden {
-    display: none; /* Скрываем боковую панель, если она скрыта */
+    display: none; // Скрываем боковую панель
   }
-  .list {
-    width: 165px; /* Ширина списка */
-    height: 555px; /* Высота списка */
-    display: flex; /* Используем flexbox */
-    flex-wrap: wrap; /* Перенос элементов */
-    align-items: center; /* Выравнивание по центру */
-    margin-top: 90px; /* Отступ сверху */
-  }
-  side-bar .logo {
-    margin-top: 80px; /* Отступ сверху для логотипа */
-  }
-  #sidebar.visible {
-    display: block; /* Показываем боковую панель, если она видима */
-    position: absolute; /* Абсолютное позиционирование */
-    z-index: 1000; /* Уровень слоя */
-    background-color: var(--white-color); /* Цвет фона боковой панели */
-    height: 100%; /* Высота на весь экран */
-  }
-}
 
-/* Медиа-запрос для экранов шириной до 1440px */
-@media (max-width: 1440px) {
-  .container {
-    width: 1110px; /* Ширина контейнера */
-  }
   header {
-    padding: 0; /* Убираем отступы у заголовка */
+    grid-column: span 12 / span 10; // Заголовок занимает 10 колонок
   }
 
-  .line {
-    width: 550px; /* Ширина линии */
+  .setting {
+    margin-left: 30px; // Отступ слева для настроек
   }
-  .btn {
-    justify-content: flex-end;
-    width: auto;
-    height: auto;
+
+  .list {
+    width: 165px; // Ширина списка
+    height: 555px; // Высота списка
+    display: flex; // Используем flexbox
+    flex-wrap: wrap; // Перенос элементов
+    align-items: center; // Выравнивание по центру
+    margin-top: 90px; // Отступ сверху
+  }
+
+  side-bar .logo {
+    margin-top: 80px; // Отступ сверху для логотипа
+  }
+
+  #sidebar.visible {
+    display: block; // Показываем боковую панель, если она видима
+    position: absolute; // Абсолютное позиционирование
+    z-index: 1000; // Уровень слоя
+    background-color: var(--white-color); // Цвет фона боковой панели
+    height: 100%; // Высота на весь экран
   }
 }
 
 /* Медиа-запрос для экранов шириной до 425px */
 @media (max-width: 450px) {
-  .container {
-    width: 325px !important; /* Ширина контейнера */
+  header {
+    height: 140px; // Высота заголовка
   }
 
-  .line {
-    width: 285px; /* Ширина линии */
-  }
-  form .img-form {
-    left: 20%; /* Положение формы изображения */
-    margin-top: 18px; /* Отступ сверху */
-  }
-  header {
-    height: 140px; /* Высота заголовка */
-  }
   .info {
-    display: none; /* Скрываем информацию */
+    display: none; // Скрываем информацию
   }
+
   nav {
-    margin-bottom: 45px; /* Отступ снизу для навигации */
+    margin-bottom: 45px; // Отступ снизу для навигации
+    justify-content: space-between;
   }
+
   .menu {
-    margin-bottom: 50px; /* Отступ снизу для меню */
-    margin-left: 29px; /* Отступ слева для меню */
+    margin-bottom: 50px; // Отступ снизу для меню
+    margin-left: 29px; // Отступ слева для меню
   }
+
   .search-inp-mobile {
-    display: block; /* Показываем мобильное поле поиска */
-    position: absolute; /* Абсолютное позиционирование */
-    top: 8%; /* Положение сверху */
-    left: 5%; /* Положение слева */
+    display: block; // Показываем мобильное поле поиска
+    position: absolute; // Абсолютное позиционирование
+    top: 4%; // Положение сверху
+    left: 10%; // Положение слева
+
+    input {
+      width: 325px; // Ширина поля ввода
+      height: 40px; // Высота поля ввода
+      border: none; // Убираем границу
+      outline: none; // Убираем обводку
+      border-radius: 40px; // Скругление углов
+      background-color: var(--background-color); // Цвет фона поля ввода
+      display: flex; // Используем flexbox
+      justify-content: center; // Выравнивание по центру
+      padding: 5px; // Отступы
+      align-items: center; // Выравнивание по центру
+
+      &::placeholder {
+        font-family: Inter; // Шрифт для плейсхолдера
+        font-size: 13px; // Размер шрифта
+        font-weight: 400; // Жирность шрифта
+        line-height: 15.73px; // Высота строки
+        color: var(--text-color); // Цвет плейсхолдера
+        padding: 50px; // Отступы
+      }
+    }
+
+    img {
+      margin-bottom: -33px; // Отступ снизу для изображения
+      margin-left: 18px; // Отступ слева для изображения
+      width: 16px; // Ширина изображения
+      height: 16px; // Высота изображения
+    }
+  }
+
+  .setting p {
+    font-family: Inter; // Шрифт для текста настроек
+    font-size: 20px; // Размер шрифта
+    font-weight: 600; // Жирность шрифта
+    line-height: 24.2px; // Высота строки
+    text-align: center; // Выравнивание текста по центру
+  }
+
+  .info-mobile img {
+    width: 35px; // Ширина изображения мобильной информации
+    height: 35px; // Высота изображения мобильной информации
+    border-radius: 50%; // Скругление углов изображения
+  }
+
+  .info-mobile {
+    display: block; // Показываем мобильную информацию
+    margin-right: 20px;
   }
   #closeIcon {
-    left: 143px; /* Положение иконки закрытия */
+    left: 181px;
   }
-  .search-inp-mobile input {
-    width: 325px; /* Ширина поля ввода */
-    height: 40px; /* Высота поля ввода */
-    border: none; /* Убираем границу */
-    outline: none; /* Убираем обводку */
-    border-radius: 40px; /* Скругление углов */
-    background-color: var(--background-color); /* Цвет фона поля ввода */
-    display: flex; /* Используем flexbox */
-    justify-content: center; /* Выравнивание по центру */
-    padding: 5px; /* Отступы */
-    align-items: center; /* Выравнивание по центру */
-  }
-  .search-inp-mobile img {
-    margin-bottom: -33px; /* Отступ снизу для изображения */
-    margin-left: 18px; /* Отступ слева для изображения */
-    width: 16px; /* Ширина изображения */
-    height: 16px; /* Высота изображения */
-  }
-  .search-inp-mobile input::placeholder {
-    font-family: Inter; /* Шрифт для плейсхолдера */
-    font-size: 13px; /* Размер шрифта */
-    font-weight: 400; /* Жирность шрифта */
-    line-height: 15.73px; /* Высота строки */
-    color: var(--text-color); /* Цвет плейсхолдера */
-    padding: 50px; /* Отступы */
-  }
-  .setting p {
-    font-family: Inter; /* Шрифт для текста настроек */
-    font-size: 20px; /* Размер шрифта */
-    font-weight: 600; /* Жирность шрифта */
-    line-height: 24.2px; /* Высота строки */
-    text-align: center; /* Выравнивание текста по центру */
-  }
-  .info-mobile img {
-    width: 35px; /* Ширина изображения мобильной информации */
-    height: 35px; /* Высота изображения мобильной информации */
-    border-radius: 50%; /* Скругление углов изображения */
-  }
-  .list {
-    margin-top: 10px; /* Отступ сверху для списка */
-  }
-  .list li a {
-    font-size: 15px; /* Размер шрифта для ссылок в списке */
-  }
-  .info-mobile {
-    display: block; /* Показываем мобильную информацию */
-  }
-  .name input,
-  .name select {
-    width: 280px; /* Ширина полей ввода и выбора */
-    height: 40px; /* Высота полей ввода и выбора */
-  }
-  .link-main a {
-    font-family: Inter; /* Шрифт для ссылок */
-    font-size: 13px; /* Размер шрифта */
-    font-weight: 500; /* Жирность шрифта */
-    line-height: 15.73px; /* Высота строки */
-  }
-  main .link-main {
-    gap: 30px; /* Промежуток между элементами */
-  }
+
   .btn {
     display: flex;
     padding-top: 30px;
     justify-content: center;
     align-items: center;
-    width: auto; /* Ширина кнопки */
-    height: auto; /* Высота кнопки */
-  }
-  .btn button {
-    width: 285px; /* Ширина кнопки */
-    height: 40px; /* Высота кнопки */
-  }
-}
+    width: auto; // Ширина кнопки
+    height: auto; // Высота кнопки
 
-/* Медиа-запрос для экранов шириной до 325px */
-@media (max-width: 325px) {
-  .search-inp-mobile input {
-    width: 287px; /* Ширина поля ввода для мобильного поиска */
-  }
-  .btn {
-    right: 0% !important; /* Положение кнопки */
+    button {
+      width: 285px; // Ширина кнопки
+      height: 40px; // Высота кнопки
+    }
   }
 }
 </style>
